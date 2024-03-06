35a41621d942b6137c8dcdb4fdb85f09f65da703
Shohin Abdulkhamidov <abd.shohin@gmail.com>
2024-03-05 19:28:45 -0800

    chore: add context

---
+++ foundation/web/context.go
@@ -0,0 +1,27 @@
+package web
+
+import (
+	"context"
+	"time"
+)
+
+type ctxKey int
+
+const key ctxKey = 1
+
+// Values represent state for each request.
+type Values struct {
+	TraceID    string
+	Now        time.Time
+	StatusCode int
+}
+
+// GetTraceID returns the trace id from the context.
+func GetTraceID(ctx context.Context) string {
+	v, ok := ctx.Value(key).(*Values)
+	if !ok {
+		return "00000000-0000-0000-0000-000000000000"
+	}
+
+	return v.TraceID
+}
